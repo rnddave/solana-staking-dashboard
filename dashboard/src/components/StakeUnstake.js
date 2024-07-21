0014b756de278cfaf5f737dd79f35e75f9d8169a
david <david@discovery.cable.virginm.net>
2024-07-21 22:16:24 +0100

    basics for stake / unstake

---
+++ dashboard/src/components/StakeUnstake.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const StakeUnstake = () => {
+    const handleStake = () => {
+        // staking here COME BACK TO THIS
+    };
+
+    const handleUnstake = () => {
+        // how to unstake will go here
+    };
+
+    return (
+        <div>
+            <h2>Stake / Unstake</h2>
+            <button onClick={handleStake}>Stake It</button>
+            <button onClick={handleUnstake}>Unstake It</button>
+        </div>
+    );
+
+};
+
+export default StakeUnstake;
+
+
+
+
