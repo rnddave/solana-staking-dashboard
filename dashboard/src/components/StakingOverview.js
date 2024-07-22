1c2a558f6beec4bdd4c12866fd4ae9e6942e0cf9
david <david@discovery.cable.virginm.net>
2024-07-22 11:13:42 +0100

    trying to catch errors related to wallet address as currently the page loads with immediate error as no wallet address even inserted yet...

--- dashboard/src/components/StakingOverview.js
+++ dashboard/src/components/StakingOverview.js
@@ -3,20 +3,39 @@ import { getBalance } from '../services/solana';
 
 const StakingOverview = ({ walletAddress }) => {
     const [balance, setBalance] = useState(0);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         const fetchBalance = async () => {
-            const balance = await getBalance(walletAddress);
-            setBalance(balance);
+
+            // user a try | catch to catch error if wallet address is invalid
+            try {
+                const balance = await getBalance(walletAddress);
+                setBalance(balance);
+                setError(null);
+            } catch (error) {
+                setError(err.message);
+            }
+            
         };
-        fetchBalance();
+
+        // if no error and IF we have a wallet, then...
+        if (walletAddress) {
+            fetchBalance();
+        }
+
     }, [walletAddress]);
 
     return (
         <div>
             <h2>Staking Overview</h2>
             <p>Wallet Address: {walletAddress}</p>
-            <p>Balance: {balance} SOL</p>
+            {error ? (
+                <p style={{ color: 'red' }}>Error: {error}</p>
+            ) : (
+                <p>Balance: {balance} SOL</p>
+            )}
+            
             {/* Staking Data goes here COME BACK TO THIS */}
         </div>
     );
--- dashboard/src/services/solana.js
+++ dashboard/src/services/solana.js
@@ -1,11 +1,23 @@
 import { Connection, PublicKey, clusterApiUrl } from '@solana/web3.js';
+// I was getting an error when page loads as Wallet Address did not contain a bs58 address
+import bs58 from 'bs58';
 
 const connection = new Connection(clusterApiUrl('mainnet-beta'));
 
 export const getBalance = async (walletAddress) => {
-    const publicKey = new PublicKey(walletAddress);
-    const balance = await connection.getBalance(publicKey);
-    return balance / 1e9; // Solana atomic unit, I think like a wei or so? 
+
+    // try|catch to prevent error on initial page load
+    try {
+        bs58.decode(walletAddress);
+    
+        const publicKey = new PublicKey(walletAddress);
+        const balance = await connection.getBalance(publicKey);
+        return balance / 1e9; // Solana atomic unit, I think like a wei or so? 
+    
+    } catch (error) {
+        console.error('Address is incorrect', error);
+        throw new Error('Wallet Address is Invalid');
+    }    
 };
 
 export const getStakeAccounts = async (walletAddress) => {
