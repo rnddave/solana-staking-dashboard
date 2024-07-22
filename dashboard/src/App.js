43292d1ba9182e1ee96e0d6266ce1cdc91b8b5a6
david <david@discovery.cable.virginm.net>
2024-07-22 11:22:57 +0100

    changes to App.js to incorp error checking

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -5,7 +5,7 @@ import RewardsTracker from "./components/RewardsTracker";
 
 function App() {
 
-  const [walletAddres, setWalletAddress] = useState('Enter Wallet Address:');
+  const [walletAddres, setWalletAddress] = useState('');
 
   return (
     <div className="App">
@@ -13,7 +13,7 @@ function App() {
       <input
         type="text"
         value={walletAddres}
-        onChange={(e) => setWalletAddress(e.target.value)}
+        onChange={(e) => setWalletAddress(e.target.value.trim())}
         placeholder="Enter Wallet Address"
       />
       <StakingOverview walletAddress={walletAddres} />
